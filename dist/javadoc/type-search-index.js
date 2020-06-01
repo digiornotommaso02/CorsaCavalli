a782c456647368a0b8ede3ce7a058343b09c9bb9
digiornotommaso02 <56291931+digiornotommaso02@users.noreply.github.com>
2020-06-01 16:58:49 +0200

    Add files via upload

---
+++ dist/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"progcorsacavalli","l":"Cavalli"},{"p":"progcorsacavalli","l":"Cavallo"},{"p":"progcorsacavalli","l":"Pista"},{"p":"progcorsacavalli","l":"ProgCorsaCavalli"}]
